01fa3760db455b2e7baf8e3a115e39207ea5317e
Edelita Valdez <edaleeta@gmail.com>
2019-12-06 12:47:16 -0500

    Completes Day 3 Part 2 in Python.

--- python/03/crossed_wires.py
+++ python/03/crossed_wires.py
@@ -1,8 +1,9 @@
 INPUT = 'input.txt'
+TEST_INPUT = 'test_input.txt'
 
 
-def create_path(instructions):
-    path = set()
+def create_path_coords(instructions, is_list=False):
+    path = [] if is_list else set()
     current_x = 0
     current_y = 0
 
@@ -21,7 +22,11 @@ def create_path(instructions):
                 current_y -= 1
             else:
                 raise Exception("Invalid direction {} found.".format(direction))
-            path.add((current_x, current_y))
+
+            if is_list:
+                path.append((current_x, current_y))
+            else:
+                path.add((current_x, current_y))
 
     return path
 
@@ -47,17 +52,31 @@ def parse_input_text(filename):
     return [path.split(',') for path in text]
 
 
+def get_sum_steps_to_intersection(intersection, path_a, path_b):
+    steps_a = path_a.index(intersection) + 1
+    steps_b = path_b.index(intersection) + 1
+    return steps_a + steps_b
+
+
 def find_solution():
     wire_a_input, wire_b_input = parse_input_text(INPUT)
 
-    path_a = create_path(wire_a_input)
-    path_b = create_path(wire_b_input)
+    path_a_coords = create_path_coords(wire_a_input)
+    path_b_coords = create_path_coords(wire_b_input)
 
-    intersections = get_intersections(path_a, path_b)
+    intersections = get_intersections(path_a_coords, path_b_coords)
     print("Intersections:", intersections)
 
     min_distance = min([get_euclidean_distance_from_origin(intersection) for intersection in intersections])
     print("Min distance is: ", min_distance)
+
+    print("Part 2:")
+
+    path_a_ordered = create_path_coords(wire_a_input, is_list=True)
+    path_b_ordered = create_path_coords(wire_b_input, is_list=True)
+
+    min_steps = min([get_sum_steps_to_intersection(intersection, path_a_ordered, path_b_ordered) for intersection in intersections])
+    print("Min steps: ", min_steps)
 
 
 find_solution()
