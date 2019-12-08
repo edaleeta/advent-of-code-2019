617cdb90912c24f57eae4c50b0f2dd0b211fe746
Edelita Valdez <edaleeta@gmail.com>
2019-12-08 15:41:25 -0500

    Completes Day 5 Part 2 in Python

--- python/05/intcode_again.py
+++ python/05/intcode_again.py
@@ -5,6 +5,7 @@ intcode = importlib.import_module('python.02.intcode', 'IntcodeMachine')
 IntcodeMachine = getattr(intcode, 'IntcodeMachine')
 INPUT = 'input.txt'
 TEST_INPUT = 'test_input.txt'
+INPUT_TEST_PART_TWO = 'test_input_part_two.txt'
 
 
 class IntcodeMachineDayFive(IntcodeMachine):
@@ -15,40 +16,33 @@ class IntcodeMachineDayFive(IntcodeMachine):
         self.curr_opcode = None
         self.curr_param_modes = None
 
+    def get_mode(self):
+        if self.curr_param_modes:
+            return self.curr_param_modes.pop()
+        # Default to 0: position mode
+        return 0
+
+    def get_value_for_param(self, param):
+        mode = self.get_mode()
+        if mode == 1:
+            return param
+        elif mode == 0:
+            return self.get_value_at_loc(param)
+        else:
+            raise Exception('Unknown mode {} found.'.format(mode))
+
     def add(self):
         """ OPCODE: 1 """
-        overwrite_loc = self.opcodes[self.cursor+3]
-        nums_to_sum = []
-        for i in range(1, 3):
-            mode = 0
-            param = self.opcodes[self.cursor+i]
-            if len(self.curr_param_modes) > 0:
-                mode = self.curr_param_modes.pop()
-
-            if mode == 0:
-                nums_to_sum.append(self.get_value_at_loc(param))
-            elif mode == 1:
-                nums_to_sum.append(param)
-
-        self.overwrite_value_at_loc(sum(nums_to_sum), overwrite_loc)
+        param_a, param_b, overwrite_loc = self.opcodes[self.cursor+1:self.cursor+4]
+        summed_params = self.get_value_for_param(param_a) + self.get_value_for_param(param_b)
+        self.overwrite_value_at_loc(summed_params, overwrite_loc)
         self.cursor += 4
 
     def multiply(self):
         """ OPCODE: 2 """
-        overwrite_loc = self.opcodes[self.cursor+3]
-        nums_to_mult = []
-
-        for i in range(1, 3):
-            mode = 0
-            param = self.opcodes[self.cursor+i]
-            if len(self.curr_param_modes) > 0:
-                mode = self.curr_param_modes.pop()
-            if mode == 0:
-                nums_to_mult.append(self.get_value_at_loc(param))
-            elif mode == 1:
-                nums_to_mult.append(param)
-
-        self.overwrite_value_at_loc(reduce(lambda x, y: x * y, nums_to_mult), overwrite_loc)
+        param_a, param_b, overwrite_loc = self.opcodes[self.cursor+1:self.cursor+4]
+        summed_params = self.get_value_for_param(param_a) * self.get_value_for_param(param_b)
+        self.overwrite_value_at_loc(summed_params, overwrite_loc)
         self.cursor += 4
 
     def save_input_value_to_position(self):
@@ -58,20 +52,51 @@ class IntcodeMachineDayFive(IntcodeMachine):
 
     def get_output_from_position(self):
         """ OPCODE: 4 """
-        mode = 0
-        if len(self.curr_param_modes) > 0:
-            mode = self.curr_param_modes.pop()
 
         param = self.opcodes[self.cursor+1]
-        output_value = None
-
-        if mode == 0:
-            output_value = self.get_value_at_loc(param)
-        elif mode == 1:
-            output_value = param
-
+        output_value = self.get_value_for_param(param)
         self.outputs.append(output_value)
         self.cursor += 2
+
+    def jump_if_true(self):
+        """ OPCODE: 5 """
+        param_a, param_b = self.opcodes[self.cursor+1:self.cursor+3]
+        test_value = self.get_value_for_param(param_a)
+
+        if test_value != 0:
+            overwrite_value = self.get_value_for_param(param_b)
+            self.cursor = overwrite_value
+        else:
+            self.cursor += 3
+
+    def jump_if_false(self):
+        """ OPCODE: 6 """
+        param_a, param_b = self.opcodes[self.cursor+1:self.cursor+3]
+        test_value = self.get_value_for_param(param_a)
+
+        if test_value == 0:
+            overwrite_value = self.get_value_for_param(param_b)
+            self.cursor = overwrite_value
+        else:
+            self.cursor += 3
+
+    def less_than(self):
+        """ OPCODE: 7 """
+        param_a, param_b, overwrite_loc = self.opcodes[self.cursor+1:self.cursor+4]
+        if self.get_value_for_param(param_a) < self.get_value_for_param(param_b):
+            self.overwrite_value_at_loc(1, overwrite_loc)
+        else:
+            self.overwrite_value_at_loc(0, overwrite_loc)
+        self.cursor += 4
+
+    def equals(self):
+        """ OPCODE: 8 """
+        param_a, param_b, overwrite_loc = self.opcodes[self.cursor+1:self.cursor+4]
+        if self.get_value_for_param(param_a) == self.get_value_for_param(param_b):
+            self.overwrite_value_at_loc(1, overwrite_loc)
+        else:
+            self.overwrite_value_at_loc(0, overwrite_loc)
+        self.cursor += 4
 
     def run_program(self):
         while self.cursor < self.get_program_length():
@@ -87,6 +112,14 @@ class IntcodeMachineDayFive(IntcodeMachine):
                 self.save_input_value_to_position()
             elif self.curr_opcode == 4:
                 self.get_output_from_position()
+            elif self.curr_opcode == 5:
+                self.jump_if_true()
+            elif self.curr_opcode == 6:
+                self.jump_if_false()
+            elif self.curr_opcode == 7:
+                self.less_than()
+            elif self.curr_opcode == 8:
+                self.equals()
             elif self.curr_opcode == 99:
                 break
             else:
@@ -99,6 +132,12 @@ def get_solution():
     m = IntcodeMachineDayFive(text, 1)
     m.run_program()
     print("Part 1 output: ", m.outputs)
+
+    with open(INPUT) as file:
+        text = file.read().strip()
+    m = IntcodeMachineDayFive(text, 5)
+    m.run_program()
+    print("Part 2 output: ", m.outputs)
 
 
 get_solution()
